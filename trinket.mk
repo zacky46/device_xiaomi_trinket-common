c2895d36b1362317542964aa06547883e9991fc2
Introdructor <yashkachave7@gmail.com>
2020-08-11 06:44:40 +0000

    trinket-common: add livedisplay support

--- trinket.mk
+++ trinket.mk
@@ -177,6 +177,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_trinket
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
